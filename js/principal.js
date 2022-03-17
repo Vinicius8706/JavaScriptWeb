03930933054d8bbefd5d8973e4317f14ab6ecb88
ViniciusFarias <fsvinicius9@gmail.com>
2022-03-17 13:00:17 -0300

    colocando condicoes js

--- js/principal.js
+++ js/principal.js
@@ -8,7 +8,17 @@ var peso = peso.textContent
 var tdAltura = paciente.querySelector(".info-altura");
 var altura = tdAltura.textContent
 
+var tdImc = paciente.querySelector(".info-imc")
 imc = peso/(altura**2)
+tdImc.textContent =imc
+
+if(peso<0){
+  console.log("Peso inválido")
+}else if(peso > 1000){
+  console.log("Peso inválido")
+}
+
+
 console.log(peso);
 console.log(tdAltura);
 console.log(imc);
