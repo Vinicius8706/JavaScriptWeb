2bfb052a5068fb64ab901a7419a638bf076dc3d6
Vinicius <fsvinicius9@gmail.com>
2022-03-20 11:48:47 -0300

    adicionando o filtrarPacientes

--- js/remover-pacientes.js
+++ js/remover-pacientes.js
@@ -1,6 +1,8 @@
-var pacientes = document.querySelectorAll(".paciente")
-
 var tabela = document.querySelector("table")
+
 tabela.addEventListener("dblclick", function (event) {
-  event.target.parentNode.remove(); //subir para o pai que foi clicado
+  event.target.parentNode.classList.add("fadeOut")
+  setTimeout(function () {
+    event.target.parentNode.remove(); //subir para o pai que foi clicado
+  },500)
 })
